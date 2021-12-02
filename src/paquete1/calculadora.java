302b77a0b5d2d8ef9ea173d399316684987114f1
arizahidalgo <a_javier.ariza.hidalgo@iespablopicasso.es>
2021-12-02 14:19:46 +0100

    Añado la multiplicacion y division

--- src/paquete1/calculadora.java
+++ src/paquete1/calculadora.java
@@ -18,6 +18,12 @@ public class calculadora {
 		case 2:
 			System.out.println(n1-n2);
 			break;
+		case 3:
+			System.out.println(n1*n2);
+			break;
+		case 4:
+			System.out.println(n1/n2);
+			break;
 
 		default:
 			break;
